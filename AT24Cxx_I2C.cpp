6033ee2d5617424aba73893fead7f772c22759e5
Benoit Rapidel <benoit.rapidel+devs@exmachina.fr>
2017-06-13 15:07:22 +0200

    Add base files

---
+++ AT24Cxx_I2C.cpp
@@ -0,0 +1,285 @@
+/* mbed simplified access to Microchip 24LCxx Serial EEPROM devices (I2C)
+ * Copyright (c) 2010-2012 ygarcia, MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+ * and associated documentation files (the "Software"), to deal in the Software without restriction, 
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or 
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <iostream>
+#include <sstream>
+
+#include "AT24Cxx_I2C.h"
+
+namespace _AT24CXX_I2C {
+
+    unsigned char AT24CXX_I2C::I2CModuleRefCounter = 0;
+
+    AT24CXX_I2C::AT24CXX_I2C(const PinName p_sda, const PinName p_scl, const unsigned char p_address, const unsigned int p_frequency) : _internalId("") {
+        if (AT24CXX_I2C::I2CModuleRefCounter != 0) {
+            error("AT24CXX_I2C: Wrong params");
+        }
+
+        _i2c_instance = new I2C(p_sda, p_scl);
+        AT24CXX_I2C::I2CModuleRefCounter += 1;
+
+        _slaveAddress = (p_address << 1) | 0xa0; // Slave address format is: 1 0 1 0 A3 A2 A1 R/W
+        _i2c_instance->frequency(p_frequency); // Set the frequency of the I2C interface
+    }
+
+    AT24CXX_I2C::~AT24CXX_I2C() {
+
+        // Release I2C instance
+        AT24CXX_I2C::I2CModuleRefCounter -= 1;
+        if (AT24CXX_I2C::I2CModuleRefCounter == 0) {
+            delete _i2c_instance;
+            _i2c_instance = NULL;
+        }
+
+    }
+
+    bool AT24CXX_I2C::erase_memory(const short p_startAddress, const int p_count, const unsigned char p_pattern) {
+
+        unsigned char ebuffer[p_count];
+        for (int i=0; i<p_count; i++)
+            ebuffer[i] = p_pattern;
+        return write(p_startAddress, ebuffer, p_count);
+    }
+
+    bool AT24CXX_I2C::write(const short p_address, const unsigned char p_byte) {
+
+        // 1.Prepare buffer
+        char i2cBuffer[3]; // Memory address + one byte of data
+        // 1.1. Memory address
+        short address = p_address + 1; // Index start to 1
+        i2cBuffer[0] = (unsigned char)(address >> 8);
+        i2cBuffer[1] = (unsigned char)((unsigned char)address & 0xff);
+        // 1.2. Datas
+        i2cBuffer[2] = p_byte;
+
+        // 2. Send I2C start + I2C address + Memory Address + Datas + I2C stop
+        int result = _i2c_instance->write(_slaveAddress, i2cBuffer, 3);
+        wait(0.02);
+
+        return (bool)(result == 0);
+    }
+
+    bool AT24CXX_I2C::write(const short p_address, const short p_short, const AT24CXX_I2C::Mode p_mode) {
+
+        // 1.Prepare buffer
+        char i2cBuffer[4]; // Memory address + one short (2 bytes)
+        // 1.1. Memory address
+        short address = p_address + 1; // Index start to 1
+        i2cBuffer[0] = (unsigned char)(address >> 8);
+        i2cBuffer[1] = (unsigned char)((unsigned char)address & 0xff);
+        // 1.2. Datas
+        if (p_mode == BigEndian) {
+            i2cBuffer[2] = (unsigned char)(p_short >> 8);
+            i2cBuffer[3] = (unsigned char)((unsigned char)p_short & 0xff);
+        } else {
+            i2cBuffer[2] = (unsigned char)((unsigned char)p_short & 0xff);
+            i2cBuffer[3] = (unsigned char)(p_short >> 8);
+        }
+
+        // 2. Send I2C start + I2C address + Memory Address + Datas + I2C stop
+        int result = _i2c_instance->write(_slaveAddress, i2cBuffer, 4);
+        wait(0.02);
+
+        return (bool)(result == 0);
+    }
+
+    bool AT24CXX_I2C::write(const short p_address, const int p_int, const AT24CXX_I2C::Mode p_mode) {
+
+        // 1.Prepare buffer
+        char i2cBuffer[6]; // Memory address + one integer (4 bytes)
+        // 1.1. Memory address
+        short address = p_address + 1; // Index start to 1
+        i2cBuffer[0] = (unsigned char)(address >> 8);
+        i2cBuffer[1] = (unsigned char)((unsigned char)address & 0xff);
+        // 1.2. Datas
+        if (p_mode == BigEndian) {
+            i2cBuffer[2] = (unsigned char)(p_int >> 24);
+            i2cBuffer[3] = (unsigned char)(p_int >> 16);
+            i2cBuffer[4] = (unsigned char)(p_int >> 8);
+            i2cBuffer[5] = (unsigned char)((unsigned char)p_int & 0xff);
+        } else {
+            i2cBuffer[2] = (unsigned char)((unsigned char)p_int & 0xff);
+            i2cBuffer[3] = (unsigned char)(p_int >> 8);
+            i2cBuffer[4] = (unsigned char)(p_int >> 16);
+            i2cBuffer[5] = (unsigned char)(p_int >> 24);
+        }
+
+        // 2. Send I2C start + I2C address + Memory Address + Datas + I2C stop
+        int result = _i2c_instance->write(_slaveAddress, i2cBuffer, 6);
+        wait(0.02);
+
+        return (bool)(result == 0);
+    }
+
+    bool AT24CXX_I2C::write(const short p_address, const char *p_datas, const int p_length) {
+
+        // 1.Prepare buffer
+        int length = (p_length == -1) ? strlen(p_datas) : p_length;
+        short address = p_address;
+
+        int result = 0;
+        for (short b=0; b<length; b+=AT24C_PAGE_SIZE) {
+            short baddress = address + b;
+            short blength = (length - b) < AT24C_PAGE_SIZE ? length - b : AT24C_PAGE_SIZE;
+#if AT24C_ADDRESS_SIZE == 1
+            char i2cBuffer[blength + 1];
+
+            i2cBuffer[0] = (unsigned char)((unsigned char)baddress & 0xff);
+            for (short i=0; i<blength; i++)
+                i2cBuffer[i + 1] = p_datas[b + i];
+
+            result += _i2c_instance->write(_slaveAddress, i2cBuffer, blength + 1);
+#elif AT24C_ADDRESS_SIZE == 2
+            char i2cBuffer[blength + 2];
+
+            i2cBuffer[0] = (unsigned char)(baddress >> 8);
+            i2cBuffer[1] = (unsigned char)((unsigned char)baddress & 0xff);
+            for (short i=0; i<blength; i++)
+                i2cBuffer[i + 2] = p_datas[b + i];
+
+            result += _i2c_instance->write(_slaveAddress, i2cBuffer, blength + 2);
+#endif
+        }
+
+        return (bool)(result == 0);
+    }
+
+    bool AT24CXX_I2C::write(const short p_address, const unsigned char *p_datas, const int p_length) {
+        return write(p_address, (const char *)p_datas, p_length);
+    }
+
+    bool AT24CXX_I2C::read(const short p_address, unsigned char * p_byte) {
+
+        // 1.Prepare buffer
+        char i2cBuffer[2];
+        // 1.1. Memory address
+        i2cBuffer[0] = (unsigned char)((unsigned char)p_address & 0xff);
+
+        // 2. Send I2C start + memory address
+        if (_i2c_instance->write(_slaveAddress, i2cBuffer, 2, true) == 0) {
+            // 2. read data + I2C stop
+            int result = _i2c_instance->read(_slaveAddress, (char *)p_byte, 1);
+
+            return (bool)(result == 0);
+        }
+
+        return false;
+    }
+
+    bool AT24CXX_I2C::read(const short p_address, short *p_short, const AT24CXX_I2C::Mode p_mode) {
+
+        // 1.Prepare buffer
+        char i2cBuffer[2];
+        // 1.1. Memory address
+        i2cBuffer[0] = (unsigned char)((unsigned char)p_address >> 8);
+        i2cBuffer[1] = (unsigned char)((unsigned char)p_address & 0xff);
+
+        // 2. Send I2C start + memory address
+        if (_i2c_instance->write(_slaveAddress, i2cBuffer, 2, true) == 0) {
+            // 2. read data + I2C stop
+            int result = _i2c_instance->read(_slaveAddress, i2cBuffer, 2);
+            if (result == 0) {
+                if (p_mode ==  BigEndian) {
+                    *p_short = (short)(i2cBuffer[0] << 8 | i2cBuffer[1]);
+                } else {
+                    *p_short = (short)(i2cBuffer[1] << 8 | i2cBuffer[0]);
+                }
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool AT24CXX_I2C::read(const short p_address, int *p_int, const AT24CXX_I2C::Mode p_mode) {
+
+        // 1.Prepare buffer
+        char i2cBuffer[4];
+        // 1.1. Memory address
+        i2cBuffer[0] = (unsigned char)(p_address >> 8);
+        i2cBuffer[1] = (unsigned char)((unsigned char)p_address & 0xff);
+
+        // 2. Send I2C start + memory address
+        if (_i2c_instance->write(_slaveAddress, i2cBuffer, 2, true) == 0) {
+            wait(0.02);
+            // 2. read data + I2C stop
+            int result = _i2c_instance->read(_slaveAddress, i2cBuffer, 4);
+            if (result == 0) {
+                wait(0.02);
+                if (p_mode ==  BigEndian) {
+                    *p_int = (int)(i2cBuffer[0] << 24 | i2cBuffer[1] << 16 | i2cBuffer[2] << 8 | i2cBuffer[3]);
+                } else {
+                    *p_int = (int)(i2cBuffer[3] << 24 | i2cBuffer[2] << 16 | i2cBuffer[1] << 8 | i2cBuffer[0]);
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        return false;
+    }
+
+    bool AT24CXX_I2C::read(const short p_address, unsigned char * p_datas, const int p_length) {
+
+        // 1.Prepare buffer
+        short address = p_address;
+        int length = 0;
+
+        if (p_length == -1) {
+            length = sizeof(p_datas);
+        } else {
+            length = p_length;
+        }
+
+        int result = 0;
+        for (short b=0; b<length; b+=AT24C_PAGE_SIZE) {
+            short baddress = address + b;
+            char blength = (length - b) < AT24C_PAGE_SIZE ? length - b : AT24C_PAGE_SIZE;
+#if AT24C_ADDRESS_SIZE == 1
+            char i2cBuffer[1];
+            i2cBuffer[0] = (unsigned char)((unsigned char)baddress & 0xff);
+
+            if (_i2c_instance->write(_slaveAddress, i2cBuffer, 1, true) == 0) {
+#elif AT24C_ADDRESS_SIZE == 2
+            char i2cBuffer[2];
+            i2cBuffer[0] = (unsigned char)((unsigned char)baddress >> 8);
+            i2cBuffer[1] = (unsigned char)((unsigned char)baddress & 0xff);
+
+            if (_i2c_instance->write(_slaveAddress, i2cBuffer, 2, true) == 0) {
+#endif
+
+                unsigned char buffer[blength];
+
+                result += _i2c_instance->read(_slaveAddress, (char *)buffer, blength);
+
+                if (result == 0) {
+                    memcpy(p_datas + b, &buffer, blength);
+                }
+            }
+        }
+
+        return (bool)(result == 0);
+
+        return false;
+    }
+
+} // End of namespace _24CXX_I2C
---
+++ AT24Cxx_I2C.h
@@ -0,0 +1,267 @@
+/* mbed simplified access to Microchip 24LCxx Serial EEPROM devices (I2C)
+ * Copyright (c) 2010-2012 ygarcia, MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+ * and associated documentation files (the "Software"), to deal in the Software without restriction, 
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or 
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef __AT24CXX_I2C_H__
+#define __AT24CXX_I2C_H__
+
+#include <string>
+#include <vector>
+
+#include "mbed.h" // Include mbed header + debug primitives. See DebugLibrary
+
+#define DEBUG       (0);
+#define DEBUG_ENTER         (0);
+#define DEBUG_LEAVE         (0);
+
+#define AT24C_PAGE_SIZE  (128)
+#define AT24C_ADDRESS_SIZE (2)
+namespace _AT24CXX_I2C {
+    /** This class provides simplified I2C access to a Microchip 24LCxx Serial EEPROM device. V0.0.0.3
+     *
+     * Note that if the LPC1768 is powered in 3.3V and Microchip 24LCxx Serial EEPROM device could be powered at 3.3V or 5V.
+     * In this case, you shall use a bi-directional level shifter for I2C-bus. Please refer to AN97055 (http://ics.nxp.com/support/documents/interface/pdf/an97055.pdf)
+     * Microchip 24LCxx Serial EEPROM device reference:
+     * - 24LC256: DS21203K
+     * - 24LC64 : DS21189D
+     *
+     * Note that for I2C details, please visit http://www.datelec.fr/fiches/I2C.htm
+     *
+     * Note that this header file include following headers:
+     * - <string>
+     * - <vector>
+     * - <mbed.h>
+     *
+     * @remark This class was validated with Tektronix TDS2014 oscilloscope in 3.3V and in mixte power mode 3.3V for mbed and 5V for the Microchip 24LCxx Serial EEPROM device
+     * @author Yann Garcia (Don't hesitate to contact me: garcia.yann@gmail.com)
+     */
+    class AT24CXX_I2C { // TODO: Add EE Polling for write methods
+        /** Reference counter used to guarentee unicity of the instance of I2C class
+         */
+        static unsigned char I2CModuleRefCounter;
+        
+        /** Device address input: A0, A1, A2 (Pins <1,3>). See DS21203K/DS21189D - Figure 5-1: Control Byte Format for address format details
+         */
+        unsigned char _slaveAddress;
+        /** An unique instance of I2C class
+         */
+        I2C *_i2c_instance;
+    public:
+        /** Memory storage mode
+         */
+        enum Mode {
+            LittleEndian, //<! Little Endian mode: 0xA0B70708 is stored as 08: MSB and A0 LSB
+            BigEndian //<! Little Endian mode: 0xA0B70708 is stored as AO: MSB and 08 LSB
+        };
+    public:
+        /** Constructor with write Protect command pin wired. Use it to manage the first I2C module on 3.3V or 5V network
+         *
+         * @param p_sda: MBed pin for SDA
+         * @param p_scl: MBed pin for SCL
+         * @param p_address: Device address input: A0, A1, A2 (Pins <1,3>)
+         * @param p_wp: MBed pin to manage write Protect input. If NC, WP is not managed, default value is NC, not connected
+         * @param p_frequency: Frequency of the I2C interface (SCL), default value is 400KHz
+         * Example:
+         * - If A1 and A2 pins are tired to Vdd and A0 is tired to Vss, address shall '00000110'B
+         * - If A0 and A1 pins are tired to Vss and A2 is tired to Vdd, address shall '00000100'B
+         */
+        AT24CXX_I2C(const PinName p_sda, const PinName p_scl, const unsigned char p_address, const unsigned int p_frequency = 200000);
+    
+        /** Destructor
+         */
+        virtual ~AT24CXX_I2C();
+
+        /** Used to return the unique instance of I2C instance
+         */
+        inline const I2C * operator * () { return (const I2C *)_i2c_instance; };
+
+        /** Erase of memory area starting at the specified address, using the specified pattern to fill the memory area
+         *
+         * @param p_startAddress The address of the memory area (from 0 to N - 1, N is the number of cells of the memory)
+         * @param p_count The size of the memory area to erase
+         * @param p_pattern The pattern value to use to fill the memory area. Default vqlue: 0x00
+         * @return true on success, false otherwise
+         * Exemple:
+         * @code
+         * ...
+         * myEEPROM.EraseMemoryArea(0, 1024); // Set to 0x00 the first 1Kb memory 
+         * ...
+         * @endcode
+         */
+        bool erase_memory(const short p_startAddress, const int p_count, const unsigned char p_pattern = 0x00);
+    
+        /** write a byte at the specified memory address
+         *
+         * @param p_address The memory address (from 0 to N - 1, N is the number of cells of the memory)
+         * @param p_byte The byte value to save
+         * @return true on success, false otherwise
+         * Exemple:
+         * @code
+         * unsigned char value = 0xaa;
+         * ...
+         * myEEPROM.write(memoryAddress, value);
+         * ...
+         * @endcode
+         */
+        bool write(const short p_address, const unsigned char p_byte);
+    
+        /** write a short at the specified memory address according to the specified mode
+         *
+         * @param p_address The memory address (from 0 to N - 1, N is the number of cells of the memory)
+         * @param p_short The short value to save
+         * @param p_mode The storage mode. Default value: BigEndian
+         * @return true on success, false otherwise
+         * Exemple:
+         * @code
+         * short value = 0xcafe;
+         * ...
+         * myEEPROM.write(memoryAddress, value, LittleEndian);
+         * ...
+         * @endcode
+         */
+        bool write(const short p_address, const short p_short, const AT24CXX_I2C::Mode p_mode = BigEndian);
+    
+        /** write an integer at the specified memory address according to the specified mode
+         *
+         * @param p_address The memory address (from 0 to N - 1, N is the number of cells of the memory)
+         * @param p_int The integer value to save
+         * @param p_mode The storage mode. Default value: BigEndian
+         * @return true on success, false otherwise
+         * Exemple:
+         * @code
+         * int value = 0xcafedeca;
+         * ...
+         * myEEPROM.write(memoryAddress, value, LittleEndian);
+         * ...
+         * @endcode
+         */
+        bool write(const short p_address, const int p_int, const AT24CXX_I2C::Mode p_mode = BigEndian);
+    
+        /** write a buffer of bytes at the specified memory address
+         *
+         * @param p_address The memory address (from 0 to N - 1, N is the number of cells of the memory)
+         * @param p_datas The buffer of bytes to save
+         * @param p_storeLength If true, store also the length of the buffer in Big Endian mode, otherwise the length will be provided by p_length2write parameter. Default value: true.
+         * @param p_length2write The number of bytes to write, -1 for all bytes. Default value: -1
+         * @return true on success, false otherwise
+         */
+        bool write(const short p_address, const unsigned char *p_datas, const int p_length2write = -1);
+    
+        /** write a buffer of characters at the specified memory address (from 0 to N - 1, N is the number of cells of the memory)
+         *
+         * Note that the length of the buffer is not saved and the string is saved in Big Endian mode
+         * @param p_address The memory address (from 0 to N - 1, N is the number of cells of the memory)
+         * @param p_datas The string to save
+         * @param p_storeLength If true, store also the length of the string in Big Endian mode, otherwise the length will be provided by p_length2write parameter. Default value: true.
+         * @param length2write The number of character to write, -1 for all characters
+         * @return true on success, false otherwise
+         */
+        bool write(const short p_address, const char *p_datas, const int p_length2write = -1);
+    
+        /** read a byte from the specified memory address
+         *
+         * @param p_address The memory address (from 0 to N - 1, N is the number of cells of the memory)
+         * @param p_byte The byte value to read
+         * @return true on success, false otherwise
+         * Exemple:
+         * @code
+         * unsigned char value;
+         * ...
+         * myEEPROM.read(memoryAddress, (unsigned char *)&value);
+         * ...
+         * @endcode
+         */
+        bool read(const short p_address, unsigned char *p_value);
+    
+        /** read a short from the specified memory address
+         *
+         * @param p_address The memory address (from 0 to N - 1, N is the number of cells of the memory)
+         * @param p_short The short value to read
+         * @return true on success, false otherwise
+         * Exemple:
+         * @code
+         * short value;
+         * ...
+         * myEEPROM.read(memoryAddress, (short *)&value);
+         * ...
+         * @endcode
+         */
+        bool read(const short p_address, short *p_short, AT24CXX_I2C::Mode p_mode = BigEndian);
+    
+        /** read an integer from the specified memory address
+         *
+         * @param p_address The memory address (from 0 to N - 1, N is the number of cells of the memory)
+         * @param p_int The integer value to read
+         * @return true on success, false otherwise
+         * Exemple:
+         * @code
+         * int value;
+         * ...
+         * myEEPROM.read(memoryAddress, (int *)&value);
+         * ...
+         * @endcode
+         */
+        bool read(const short p_address, int *p_int, AT24CXX_I2C::Mode p_mode = BigEndian);
+    
+        /** read a buffer of characters from the specified memory address and store it into a string object
+         *
+         * Note that the size of the string object is used for the number of characters to read
+         * @param p_address The memory address (from 0 to N - 1, N is the number of cells of the memory)
+         * @param p_datas The buffer to fill
+         * @param p_readLengthFirst If true, read the length first and p_length2write parameter is ignored, otherwise the length is provided by p_length2write parameter. Default value: true
+         * @param p_length2read The number of character to read, -1 to use the size of the buffer
+         * @return true on success, false otherwise
+         * Exemple:
+         * @code
+         * std::string readtext;
+         * ...
+         * myEEPROM.read(memoryAddress, readtext);
+         * ...
+         * @endcode
+         */
+        bool read(const short p_address, unsigned char *p_datas, const int p_length2read = -1);
+
+    
+#if defined(__DEBUG)
+        /** Dump a memory area
+         * 
+         * Note that this method is available only on debug mode
+         * @param p_address The memory address (from 0 to N - 1, N is the number of cells of the memory)
+         * @param p_count The number of bytes toi dump
+         * @return true on success, false otherwise
+         */
+        void DumpMemoryArea(const int p_address, const int p_count);
+        /** For debug purpose only
+         */
+        inline std::string & ToString() { return _internalId; };
+#else // __DEBUG
+        inline void DumpMemoryArea(const int p_address, const int p_count) {};
+#endif // _DEBUG
+        
+    private:
+        /** Internal reference identifier
+         */
+        std::string _internalId;
+
+    }; // End of class AT24CXX_I2C
+
+} // End of namespace _AT24CXX_I2C
+
+using namespace _AT24CXX_I2C;
+
+#endif // __AT24CXX_I2C_H__
